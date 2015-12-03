f926391e72ed36bc9dd5bf3d26ef7b0385e52357
Brian Cairl <briancairl@aol.com>
2015-12-02 21:45:59 -0500

    Bug in viewscan for mapped rendering. Cleanfig mod'd

--- io/cleanfig.m
+++ io/cleanfig.m
@@ -1,6 +1,20 @@
-function cleanfig(f,color,varargin)
+function cleanfig(f,varargin)
     a = get(f,'CurrentAxes');
     
+    viewsize = -1;
+    color    = 'k';
+    
+    for idx = 1:2:numel(varargin)
+        if  strcmpi(varargin{idx},'VIEWSIZE')
+            viewsize = varargin{idx+1};
+        elseif  strcmpi(varargin{idx},'COLOR')
+            color = varargin{idx+1}; 
+        else
+            set(f,varargin{idx},varargin{idx+1});
+        end
+    end
+    
+
     set(a,'XTick',[])
     set(a,'YTick',[])
     set(a,'ZTick',[])
@@ -12,9 +26,9 @@ function cleanfig(f,color,varargin)
     set(a,'Color',color)
     set(f,'Color',color)
     
-    for idx = 1:2:numel(varargin)
-        set(f,varargin{idx},varargin{idx+1});
+    if viewsize < 0
+        axis(a,'equal');
+    else
+        axis(viewsize*[-1,1,-1,1,-1,1]);
     end
-    
-    axis(a,'equal')
 end
